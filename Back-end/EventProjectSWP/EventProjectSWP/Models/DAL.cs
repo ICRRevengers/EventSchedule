88ac8de1efda43cc4563c155b40a1f27b412420c
thoanglong <thoanglong94@gmail.com>
2022-06-19 22:52:58 +0700

    unfinish code

---
+++ Back-end/EventProjectSWP/EventProjectSWP/Models/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using EventProjectSWP.Models;
+namespace EventProjectSWP.Models
+{
+    public class DAL
+    {
+        SqlDataAdapter da = null;
+        DataTable dt = null;
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=EventSchedule;User Id=sa;password=12345");
+        // string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
+        public string GetLogin(Club login)
+        {
+            da = new SqlDataAdapter("Select club_name, club_phone from tblClub where club_email = '" + login.ClubEmail + "' and club_password = '" + login.ClubPassword + "' ", con);
+            dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return "logged in";
+            else
+                return "login fail";
+        }
+    }
+}
