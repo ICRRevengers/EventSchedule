d9a93a10bd0e4ca9ee1b554133299706491d5c85
Tan Diu <duytan22032001@gmail.com>
2022-06-19 22:57:17 +0700

    Image

---
+++ Back-end/EventProjectSWP/EventProjectSWP/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace EventProjectSWP.Models
+{
+    public class Image
+    {
+        public string ImageId { get; set; }
+        public IFormFile ImageUrl { get; set; }
+        public string EventId { get; set; }
+
+    }
+}
