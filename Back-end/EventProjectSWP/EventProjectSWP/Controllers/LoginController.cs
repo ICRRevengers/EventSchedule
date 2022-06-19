5900f3ae75d6af04401d1049511251fa9f21daac
thoanglong <thoanglong94@gmail.com>
2022-06-19 22:51:49 +0700

    Un-finish code
    will finish later

---
+++ Back-end/EventProjectSWP/EventProjectSWP/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using EventProjectSWP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventProjectSWP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("api/values/GetLogin")]
+        [HttpPost]
+        public string GetLogin(Club login)
+        {
+            /*BAL bal = new BAL();
+            string response = bal.GetLogin(login);
+            return response;*/
+            SqlDataAdapter da = new SqlDataAdapter();
+            da = new SqlDataAdapter("Select club_name, club_phone from tblClub where club_email = '" + login.ClubEmail + "' and club_password = '" + login.ClubPassword + "' ", con);
+           DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return "logged in";
+            else
+                return "login fail";
+        }
+
+
+
+        [HttpGet("login-by-admin-blub")]
+        public JsonResult GetClubByName(Club login)
+        {
+            string query = @"Select * from tblClub where club_email = '" + login.ClubEmail + "' and club_password = '" + login.ClubPassword + "' ";
+
+           /* SqlDataAdapter da = new SqlDataAdapter();
+            da = new SqlDataAdapter("Select club_name, club_phone from tblClub where club_email = '" + login.ClubEmail + "' and club_password = '" + login.ClubPassword + "' ", con);*/
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@club_email", login.ClubEmail);
+                    myCommand.Parameters.AddWithValue("@club_password", login.ClubPassword);
+                    myReader = myCommand.ExecuteReader();
+                    //table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Back-end/EventProjectSWP/EventProjectSWP/Models/BAL.cs
@@ -0,0 +1,12 @@
+﻿namespace EventProjectSWP.Models
+{
+    public class BAL
+    {
+        public string GetLogin(Club login)
+        {
+            DAL dal = new DAL();    
+            string response = dal.GetLogin(login);
+            return response;
+        }
+    }
+}
