8cae5316622a9c956545c540401373ed88f1f808
Hồng Hạnh <hanhchse150754@fpt.edu.vn>
2022-06-19 22:13:25 +0700

    add carousel event

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -7,22 +7,20 @@ class Login extends Component {
             <>
                 <div className="login row mr-0 ml-0 ">
                     <form className="col-12 col-md-6 text-center">
-                        <p className="col-12">If you are <strong>club's admin</strong>, login here</p>
+                        <p className="col-12">Nếu bạn là <strong>quản trị viên</strong>, đăng nhập ở đây</p>
                         <div className="form-group row">
                             <div className="form-group col-12 col-md-6 offset-md-3">
-                                <label className="sr-only" htmlFor="inputEmail">Admin account</label>
                                 <input type="email" className="form-control" id="inputAccount"
-                                    placeholder="Admin account" />
+                                    placeholder="Tài khoản" />
                             </div>
                             <div className="form-group col-12 col-md-6 offset-md-3">
-                                <label className="sr-only" htmlFor="inputPassword">Password</label>
                                 <input type="password" className="form-control" id="inputPassword"
-                                    placeholder="Password" />
+                                    placeholder="Mật khẩu" />
                             </div>
                         </div>
                     </form>
                     <div className="col-12 col-md-6 text-center">
-                        <p className="col-12 pl-0">If you are <strong>student</strong>, login with fpt.edu.vn</p>
+                        <p className="col-12 pl-0">Nếu bạn là <strong>sinh viên</strong>, đăng nhập với fpt.edu.vn</p>
                         <GoogleButton className="googleButton offset-2 offset-md-3"
                             onClick={() => { console.log('Google button clicked') }}
                         />
