e59ee252f9a3bb647b00f0a78bd8c05da09349d4
Hồng Hạnh <hanhchse150754@fpt.edu.vn>
2022-06-20 13:55:13 +0700

    add cardevent

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
       <div className="App">
         <Main />
       </div>
-      </BrowserRouter>
-    );
-  }
+    </BrowserRouter>
+  );
 }
 
 export default App;
