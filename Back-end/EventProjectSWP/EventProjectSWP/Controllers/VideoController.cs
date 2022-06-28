dcdfcb630855e57b551c189028180595b123dfb8
Tan Diu <duytan22032001@gmail.com>
2022-06-28 20:44:30 +0700

    a

--- Back-end/EventProjectSWP/EventProjectSWP/Controllers/VideoController.cs
+++ Back-end/EventProjectSWP/EventProjectSWP/Controllers/VideoController.cs
@@ -40,11 +40,6 @@ namespace EventProjectSWP.Controllers
             }
             return new JsonResult("Succeesful");
         }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> backend-Tan200k
         [HttpGet("get-video")]
         public JsonResult Get()
         {
