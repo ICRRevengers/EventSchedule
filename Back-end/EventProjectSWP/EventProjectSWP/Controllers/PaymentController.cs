c146dbf8e664ad809168d5f82093b19792fe50a5
Tan Diu <duytan22032001@gmail.com>
2022-06-29 14:31:29 +0700

    CRUD payment cơ bản

---
+++ Back-end/EventProjectSWP/EventProjectSWP/Controllers/PaymentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EventProjectSWP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public PaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("get-Payment")]
+        public JsonResult GetPayment()
+        {
+            string query = @"select * from tblPayment";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPut("update-payment")]
+        public JsonResult Put(bool status, string id)
+        {
+            string query = @"update tblEventParticipated set payment_status = @payment_status where users_id =@users_id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@payment_status", status);
+                    myCommand.Parameters.AddWithValue("@users_id", id);
+                    myReader = myCommand.ExecuteReader();
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Succeesful");
+        }
+    }
+}
---
+++ Back-end/EventProjectSWP/EventProjectSWP/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace EventProjectSWP.Models
+{
+    public class Payment
+    {
+        public string PaymentUrl { get; set; }
+        public int PaymentFee { get; set; }
+        public string PaymentDetail { get; set; }
+        public int AdminID { get; set; }
+
+    }
+}
