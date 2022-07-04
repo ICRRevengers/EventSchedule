5fdb94bdea2cbd8a9f0caae8dad15518168d4461
Unknown <thoanglong94@gmail.com>
2022-07-04 19:06:49 +0700

    small fix bug

--- Back-end/EventProjectSWP/EventProjectSWP/Controllers/EventParticipatedController.cs
+++ Back-end/EventProjectSWP/EventProjectSWP/Controllers/EventParticipatedController.cs
@@ -18,36 +18,6 @@ namespace EventProjectSWP.Controllers
             _configuration = configuration;
         }
 
-
-        [HttpPut("update-payment")]
-        public JsonResult Put(bool status, string id)
-        {
-            string query = @"update tblEventParticipated set payment_status = @payment_status where users_id =@users_id";
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@payment_status", status);
-                    myCommand.Parameters.AddWithValue("@users_id", id);
-                    myReader = myCommand.ExecuteReader();
-                    myReader.Close();
-                    myCon.Close();
-
-                }
-            }
-            return new JsonResult("Succeesful");
-        }
-
-        [HttpGet("get-list-event-participated")]
-        public JsonResult Get()
-        {
-            string query = @"select event_id , users_id, date_participated , payment_status from tblEventParticipated";
-
-        
         [HttpGet("get-userinfo-join-event")]
         // lấy tất cả thông tin người dùng đã tham gia event
         public JsonResult Get()
@@ -56,7 +26,6 @@ namespace EventProjectSWP.Controllers
 from tblEventParticipated EP, tblUser U
 where Ep.users_id = U.users_id";
 
-
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
             SqlDataReader myReader;
@@ -75,12 +44,6 @@ where Ep.users_id = U.users_id";
             return new JsonResult(table);
         }
 
-<<<<<<< HEAD
-        [HttpGet("get-list-user-join-event-participated")]
-        public JsonResult GetUserEvent(string id)
-        {
-            string query = @"select event_id , date_participated , payment_status from tblEventParticipated where users_id = @users_id";
-=======
         [HttpGet("get-all-event-i-joined")]
         // Lấy tất cả event mà 1 user tham gia 
         public JsonResult GetUserEvent(string id)
@@ -88,7 +51,6 @@ where Ep.users_id = U.users_id";
             string query = @"select U.users_id,users_name, users_phone,users_address,users_email,event_id,date_participated,payment_status
 from tblEventParticipated EP, tblUser U
 where Ep.users_id = U.users_id and U.users_id = @users_id";
->>>>>>> main
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
             SqlDataReader myReader;
@@ -108,12 +70,6 @@ where Ep.users_id = U.users_id and U.users_id = @users_id";
             return new JsonResult(table);
         }
 
-<<<<<<< HEAD
-        [HttpGet("get-user-list-from-event")]
-        public JsonResult GetUserParticipatedEvent(string id)
-        {
-            string query = @"select users_id , date_participated, payment_status from tblEventParticipated where event_id = @event_id";
-=======
         [HttpGet("get-user-list-from-an-event")]
         // Lấy tất cả user từ 1 event
         public JsonResult GetUserParticipatedEvent(string id)
@@ -122,7 +78,6 @@ where Ep.users_id = U.users_id and U.users_id = @users_id";
 from tblEventParticipated EP, tblUser U
 where Ep.users_id = U.users_id and EP.event_id = @event_id
 ";
->>>>>>> main
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
             SqlDataReader myReader;
