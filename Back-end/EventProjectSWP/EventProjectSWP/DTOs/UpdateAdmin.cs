b8eac5ca3d8f05500a7536509cd1cd28cc3a3aa6
thoanglong <thoanglong94@gmail.com>
2022-07-08 09:30:09 +0700

    small code adjust

--- Back-end/EventProjectSWP/EventProjectSWP/DTOs/UpdateAdmin.cs
+++ Back-end/EventProjectSWP/EventProjectSWP/DTOs/UpdateAdmin.cs
@@ -2,7 +2,7 @@
 {
     public class UpdateAdmin
     {
-        public int AdminID { get; set; }
+        //public int AdminID { get; set; }
         public string AdminName { get; set; }
         public string AdminPhone { get; set; }
         public string AdminPassword { get; set; }
