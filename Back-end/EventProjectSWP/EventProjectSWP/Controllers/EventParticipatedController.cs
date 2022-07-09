e80373ff2697c850d2a16dee550170508eb2a29c
thoanglong <thoanglong94@gmail.com>
2022-07-09 17:34:27 +0700

    add user join event fix bug

--- Back-end/EventProjectSWP/EventProjectSWP/Controllers/EventParticipatedController.cs
+++ Back-end/EventProjectSWP/EventProjectSWP/Controllers/EventParticipatedController.cs
@@ -150,11 +150,11 @@ namespace EventProjectSWP.Controllers
 
 
         [HttpPost("add-user-join-event")]
-        public IActionResult Post(AddUserJoinEvent EventParticipated)
+        public IActionResult Post(EventParticipated EventParticipated)
         {
             try
             {
-                string query = @"insert into tblEventParticipated(event_id,users_id,date_participated, payment_status, users_status) values(@event_id,@users_id,@date_participated, 'false', 'false')";        
+                string query = @"insert into tblEventParticipated(event_id,users_id,date_participated) values(@event_id,@users_id,@date_participated)";
                 string sqlDataSource = _configuration.GetConnectionString("EventAppConn");
                 SqlDataReader myReader;
                 using (SqlConnection myCon = new SqlConnection(sqlDataSource))
